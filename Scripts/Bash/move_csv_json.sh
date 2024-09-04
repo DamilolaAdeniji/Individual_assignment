5be29e2db1b2fc8a2b326447781338054529903b
DamilolaAdeniji <dammy_adeniji@yahoo.com>
2024-09-04 21:53:04 +0100

    added a read_me file and fixed the script that moves csv files

--- Scripts/Bash/move_csv_json.sh
+++ Scripts/Bash/move_csv_json.sh
@@ -1,10 +1,19 @@
 #!/bin/zsh
 
+# go up to directories
+
+cd ..
+cd ..
+
 touch sample1.csv
 touch sample2.csv
 touch sample1.json
 touch sample2.json
 
+echo "CSV and json files have been created successfully"
+
 mkdir -p json_and_CSV
 mv *.csv json_and_CSV
 mv *.json json_and_CSV
+
+echo "All csv and json files have been moved successfully"
